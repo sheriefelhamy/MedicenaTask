8238a760d047f7e529109b9d65c534bd1b9dd8c2
sheriefelhamy <sherief.elhamy7@gmail.com>
2026-05-09 20:59:16 +0300

    removed default data file

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,10 @@
 import env from "./config/env.js";
 import { connectToDatabase } from "./config/db.js";
-import { seedIfEmpty } from "./seed/defaultData.js";
 import { createApp } from "./app.js";
 
 async function start() {
   await connectToDatabase(env.mongoUri);
-  await seedIfEmpty();
-
+ 
   const app = createApp();
   app.listen(env.port, () => {
     console.log(`Medicena API running on port ${env.port}`);
